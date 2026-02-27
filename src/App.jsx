43dbde1f21d965fb7dbbd337c5408942939527ad
nabilersyad <nabile95@hotmail.com>
2026-02-27 22:03:55 +0800

    analytics track

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Home from './pages/Home.jsx'
 import BazaarDetail from './pages/BazaarDetail.jsx'
 import SubmitBazaar from './pages/SubmitBazaar.jsx'
 import Admin from './pages/Admin.jsx'
-
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/bazaar/:id" element={<BazaarDetail />} />
